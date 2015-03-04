e78400aa24a343ba14237ee775924841aaea266c
Rob Stinson <r.stinson11@gmail.com>
2015-03-04 11:24:01 +1000

    Layout 404, archive and search pages. Create Home page template.

--- home.php
+++ home.php
@@ -1,12 +1,6 @@
 <?php
 /**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ * The Page template file.
  *
  * @package NXI
  */
@@ -15,7 +9,7 @@ get_header(); ?>
 
 <div class="container">
 	<div class="row">
-		<div id="primary" class="content-area col-sm-9">
+		<div id="primary" class="content-area col-sm-12">
 			<main id="main" class="site-main" role="main">
 
 				<?php if ( have_posts() ) : ?>
@@ -44,7 +38,6 @@ get_header(); ?>
 			</main><!-- #main -->
 		</div><!-- #primary -->
 
-		<?php get_sidebar(); ?>
 
 	</div>
 </div>
